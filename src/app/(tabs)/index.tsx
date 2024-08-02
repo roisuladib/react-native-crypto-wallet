f35b5794817374816a5e444a316594da815e71df
Muhammad Roisul Adib <99470918+roisuladib@users.noreply.github.com>
2024-08-02 19:34:37 +0700

    initial: commit 🚀

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -4,8 +4,18 @@ import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import { client } from '@/config';
+import { useEffect, useState } from 'react';
 
 export default function HomeScreen() {
+   const [blockNumber, setBlockNumber] = useState<bigint>()
+   useEffect(() => {
+      (async () => {
+         const blockNumber = await client.getBlockNumber()
+         setBlockNumber(blockNumber)
+      })()
+   }, []);
+
   return (
     <ParallaxScrollView
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
@@ -17,6 +27,7 @@ export default function HomeScreen() {
       }>
       <ThemedView style={styles.titleContainer}>
         <ThemedText type="title">Welcome!</ThemedText>
+        <ThemedText type="title">{blockNumber?.toLocaleString()}</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
---
+++ src/config/index.ts
@@ -0,0 +1,8 @@
+import { createPublicClient, http } from 'viem';
+import { mainnet } from 'viem/chains'
+
+
+export const client = createPublicClient({
+   chain: mainnet,
+   transport: http()
+});
